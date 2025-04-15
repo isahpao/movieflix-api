649e17b8fad51cb355c8133766a1ef6b3779afc0
Isabelah Campos <115017351+isahpao@users.noreply.github.com>
2025-04-15 18:05:47 -0300

    adicionada atualização de filmes

--- src/server.ts
+++ src/server.ts
@@ -45,6 +45,28 @@ try {
 res.status(201).send();
 });
 
+app.put("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+    try{
+        const movie = await prisma.movie.findUnique({
+          where: { id },
+        });
+     
+        if (!movie) {
+          return res.status(404).send({ message: "Filme não encontrado" });
+        }
+     
+        const data = { ...req.body };
+        data.release_date = data.release_date
+          ? new Date(data.release_date)
+          : undefined;
+     
+          await prisma.movie.update({ where: { id }, data });
+       }catch(error){
+        return res.status(500).send({ message: "Falha ao atualizar o registro" });
+       }
+       res.status(200).send();
+     });
 
 
 app.listen(port, () => {

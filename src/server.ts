9a74243de0d7fd0df4c3cce080680ead1fe77126
Isabelah Campos <115017351+isahpao@users.noreply.github.com>
2025-04-16 16:51:52 -0300

    adicionada remoção de filme

--- src/server.ts
+++ src/server.ts
@@ -69,6 +69,30 @@ app.put("/movies/:id", async (req, res) => {
      });
 
 
+     app.delete('/movies/:id', async (req, res) => {
+        const id = Number(req.params.id);
+     
+     
+        try{
+          const movie = await prisma.movie.findUnique({
+          where: { id }})
+     
+      
+     
+          if (!movie) {
+             return res.status(404).send({ message: 'Filme não encontrado' });
+          }
+     
+            await prisma.movie.delete({ where: { id }});
+        
+          }catch(error) {
+            res.status(500).send({ message: 'Falha ao remover o registro' });
+          }
+        
+      res.status(200).send();
+     });
+
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
